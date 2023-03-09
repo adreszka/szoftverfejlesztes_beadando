e234b586b9e8df3784e70fcc79fb7b2ba8d0022f
Adrián <adreszka2001@gmail.com>
2023-03-09 10:11:47 +0100

    Auto class modositva

--- autokereskedes/auto.h
+++ autokereskedes/auto.h
@@ -1,11 +1,34 @@
 #ifndef AUTO_H
 #define AUTO_H
 
+#include <string>
+
+using namespace std;
 
 class Auto
 {
+    string rendszam;
+    string marka;
+    string tipus;
+    string *felszerelesek; // tömb
+    string kialakitas;
+    int ar;
+    int napidij;
+    string szin;
+    int csomagtarto_meret;
+    string uzemanyag;
+    int evjarat;
+    int motor_teljesitmeny;
+    int hengerutartalom;
+    string sebessegvalto;
+    string hajtas;
 public:
-    Auto();
+    Auto(const string& rendszam, const string& marka, const string& tipus,
+         string& felszerelesek, const string& kialakitas, int ar, int napidij,
+         const string& szin, int csomagtarto_meret, const string& uzemanyag,
+         int evjarat, int motor_teljesitmeny, int hengerutartalom,
+         const string& sebessegvalto, const string& hajtas
+         );
     // leírásokat tárol az autóról
 };
 
