3fd9a940ed0df40470abbf9b65ba3cc16d47b59f
Andris-tech <huszar.andris2002@gmail.com>
2023-03-13 17:14:48 +0100

    Tarolo frissítve van

--- autokereskedes/tarolo.h
+++ autokereskedes/tarolo.h
@@ -5,18 +5,23 @@
 #include <iostream>
 
 #include "auto.h"
+#include "regisztraltfelhasznalo.h"
 
 using namespace std;
 
 class Tarolo
 {
     list<Auto> autok;
+    RegisztraltFelhasznalo felhasznalo;
     Tarolo()=default;
     Tarolo(const Tarolo& other)=default;
 public:
     static Tarolo& getObjektum();
     void hozzaAd(const Auto& a);
-    //void kiirTeszt();
+    void kiirTeszt();
+    list<Auto> getAutok() const;
+    RegisztraltFelhasznalo getFelhasznalo() const;
+    void setFelhasznalo(const RegisztraltFelhasznalo &newFelhasznalo);
 };
 
 #endif // TAROLO_H
