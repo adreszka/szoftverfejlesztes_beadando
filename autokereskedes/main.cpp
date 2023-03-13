b59910880eab5f6f991fa92d52828d53d1dc793a
Ákos Koszták <kokoszta77@gmail.com>
2023-03-13 17:05:04 +0100

    GUI baszaxások

--- autokereskedes/main.cpp
+++ autokereskedes/main.cpp
@@ -2,8 +2,13 @@
 
 #include <QApplication>
 
+#include "adatbazis.h"
+
 int main(int argc, char *argv[])
 {
+    Adatbazis adatb;
+    adatb.csatlakozas("autokereskedes.sqlite");
+    adatb.autokBeolvas();
     QApplication a(argc, argv);
     Dialog w;
     w.show();
